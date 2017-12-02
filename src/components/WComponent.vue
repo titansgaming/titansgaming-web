958af6595216423d7c9c1c2ad6b5096d25031836
Will Moggridge <will@moggridge.com>
2017-12-02 18:49:33 +0000

    Fix up WComponent

--- src/components/WComponent.vue
+++ src/components/WComponent.vue
@@ -1,25 +1,22 @@
 <style lang="stylus" scoped>
-  {{dasd}}
 </style>
 
 <template lang="pug">
-  DIV.test Hi
 </template>
 
 <script type="text/javascript">
   export default {
     name: 'WComponent',
-    mixins: [],
-    extends: {},
     components: {},
+    extends: {},
+    mixins: [],
 
-    props: {
-    },
-    data () {},
+    props: {},
+    data() {},
     computed: {},
     watch: {},
-    methods: {},
     beforeCreate() {},
     mounted() {},
+    methods: {},
   };
 </script>
