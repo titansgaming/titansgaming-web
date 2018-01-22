0e20b5acf2fff6d5d75e9484619014f8ddb73746
Will Moggridge <will@moggridge.com>
2018-01-22 23:45:25 +0000

    Install NuxtJS

---
+++ nuxt.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  srcDir: 'src/',
+  head: {
+    title: 'Titans Gaming',
+    titleTemplate: '%s | TG',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+  },
+}
---
+++ src/pages/index.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  div
+    page-meta(title="Home")
+
+    h1 Welcome to Titans Gaming
+</template>
+
+<style lang="stylus" scoped>
+  h1, h2
+    font-weight: normal
+  ul
+    list-style-type: none
+    padding: 0
+  li
+    display: inline-block
+    margin: 0 10px
+  a
+    color: #42b983
+</style>
+
+<script>
+  export default {
+    name: 'home',
+    data() {
+      return {
+        title: 'Hello World!',
+      }
+    },
+    head() {
+      return {
+        // title: this.title,
+        meta: [
+          {
+            vmid: 'description',
+            name: 'description',
+            content: 'Titans Gaming website',
+          },
+        ],
+      };
+    },
+  };
+</script>
