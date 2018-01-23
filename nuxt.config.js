a0089301f5bfe6bc2326f48edc05c9766898fc5b
Will Moggridge <will@moggridge.com>
2018-01-23 00:22:46 +0000

    More Nuxt, Stylus settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
+const axisStylusPlugin = require('axis')
+const path = require('path')
+const webpack = require('webpack');
+
 module.exports = {
   srcDir: 'src/',
   head: {
@@ -10,5 +14,27 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
+  },
+  loading: { color: '#3B8070' },
+  // modules: ['~modules/typescript.ts'],
+
+  build: {
+    extend (config, { isDev, isClient }) {
+      config.plugins = [
+        ...config.plugins,
+        new webpack.LoaderOptionsPlugin({
+          options: {
+            stylus: {
+              import: [
+                path.join(__dirname, 'src', 'styles') + '/**/*.styl',
+              ],
+              use: [
+                axisStylusPlugin(),
+              ],
+            },
+          },
+        })
+      ];
+    },
   },
 }
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,3 +1,9 @@
 <template>
   <nuxt/>
 </template>
+
+<style lang="stylus">
+  body
+    background-color: #2c3e50;
+    color: #ffffff;
+</style>
